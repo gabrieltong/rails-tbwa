f778ea12acb46799dceb708055ff639620af94df
gabrieltong <tongyangmath@gmail.com>
2013-08-15 16:12:26 +0800

    init

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ Tbwa::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'records'
+  root :to => 'records#index'
 
   # See how all your routes lay out with "rake routes"
 
